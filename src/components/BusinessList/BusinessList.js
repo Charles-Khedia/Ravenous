407a824efa2517b44f8f2798608fb6e012347607
Charles Khedia <charles1971992@gmail.com>
2021-07-01 15:47:17 -0400

    Adding all files

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './BusinessList';
+import Business from '../Business/Business';
+import './BusinessList.css'
+
+class BusinessList extends React.Component {
+    render () {
+        return (
+            <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            
+          </div>
+        )
+    }
+}
+
+export default BusinessList;
