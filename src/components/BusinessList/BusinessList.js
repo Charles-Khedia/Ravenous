3a6aa329390c7538ea127fbf197827f663f2a4cf
Charles Khedia <charles1971992@gmail.com>
2021-07-20 16:39:36 -0400

    Update components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,19 +4,17 @@ import Business from '../Business/Business';
 import './BusinessList.css'
 
 class BusinessList extends React.Component {
-    render () {
-        return (
-            <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            
-          </div>
-        )
-    }
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map((business) => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default BusinessList;
